f1af8af4b20d2066bb8b7169296f6d9e61bdb496
marypaduano <maria.paduano@gmail.com>
2023-03-16 09:53:21 +0100

    fix section newsletter

--- src/components/InputMail.vue
+++ src/components/InputMail.vue
@@ -1,37 +1,60 @@
 <template>
     <section>
-        <div class="row">
-        <h3 class="title">Subscribe To Our Newsletter</h3>
-        <form class="newsletter">
-            <input type="email" class="email" name="email" placeholder="Insert your Email*">
-            <button type="submit">Send</button>
-        </form>
-    </div>
+        <div class="container">
+            <div>
+                <h3 class="title">Subscribe To Our Newsletter</h3>
+            <div class="row">
+                <form class="newsletter">
+                    <input type="email" class="email" name="email" placeholder="Insert your Email*">
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+            </div>
+        </div>
     </section>
 </template>
   
   
 <style lang="scss" scoped>
-
-section{
-    display: flex;
-    flex-direction: column;
-    gap: 25px;
-    align-items: center;
+section {
     background-color: rgb(242, 242, 242);
 }
 
-.newsletter{
+.title{
+    text-align: center;
+    padding-bottom: 20px;
+}
+
+.newsletter {
     display: flex;
     gap: 25px;
 }
+
+.row{
+    display: flex;
+    justify-content: center;
+}
+
 .title {
- font-size: 32px;
+    font-size: 32px;
+    padding-bottom: 30px;
 
 }
-.email{
-    padding: 15px 40px;
+
+.email {
+    padding: 15px 300px 15px 5px;
 }
 
-
+button{
+    color: white;
+    background-color: rgb(25, 25, 25);
+    padding: 12px 60px;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 16px;
+    cursor: pointer;
+    &:hover, &:active{
+        background-color: salmon;
+    }
+}
 </style>
